b7887604e80f43de933f263f3148afd22e8a0d0e
leizongmin <leizongmin@gmail.com>
2012-09-18 23:23:16 +0800

    基本代码及测试

---
+++ index.js
@@ -0,0 +1,181 @@
+/**
+ * 过滤XSS攻击
+ *
+ * @author 老雷<leizongmin@gmail.com>
+ */
+
+/*
+ * 默认HTML标签白名单
+ * 标签名=>属性列表
+ */
+var defaultWhiteList = {
+  h1:     ['style', 'class'],
+  h2:     ['style', 'class'],
+  h3:     ['style', 'class'],
+  h4:     ['style', 'class'],
+  h5:     ['style', 'class'],
+  hr:     ['style', 'class'],
+  span:   ['style', 'class'],
+  strong: ['style', 'class'],
+  b:      ['style', 'class'],
+  i:      ['style', 'class'],
+  p:      ['style', 'class'],
+  pre:    ['style', 'class'],
+  code:   ['style', 'class'],
+  a:      ['style', 'class', 'target', 'href', 'title'],
+  img:    ['style', 'class', 'src', 'alt', 'title'],
+  div:    ['style', 'class'],
+  table:  ['style', 'class', 'width', 'border'],
+  tr:     ['style', 'class'],
+  td:     ['style', 'class', 'width', 'colspan'],
+  th:     ['style', 'class', 'width', 'colspan'],
+  tbody:  ['style', 'class'],
+};
+
+
+/**
+ * XSS过滤
+ *
+ * @param {string} html 要过滤的HTML代码
+ * @param {object} whiteList 白名单，若不指定，则使用默认的
+ * @return {string}
+ */
+var xss = module.exports = function (html, whiteList) {
+  'use strict';
+
+  whiteList = whiteList || defaultWhiteList;
+  var rethtml = '';
+  var lastPos = 0;
+  var tagStart = false;
+  var quoteStart = false;
+
+  /**
+   * 转换<>为&lt; &gt
+   */
+  var noTag = function (text) {
+    return text.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  }
+
+  /**
+   * 过滤不合法的属性
+   */
+  var filterAttributes = function (attrs, whites) {
+    var lastPos = 0;
+    var _attrs = [];
+    var tmpName = false;
+    function addAttr (name, value) {
+      name = name.toLowerCase().trim();
+      if (value) {
+        value = value.trim();
+        // 值中不能出现</script>
+        value = value.replace(/<\/script>/img, '&lt;/script&gt;');
+      }
+      if (whites.indexOf(name) !== -1) {
+        _attrs.push(name + (value ? '=' + value : ''));
+      }
+    }
+    for (var i = 0, len = attrs.length; i < len; i++) {
+      var c = attrs[i];
+      if (c === '=') {
+        tmpName = attrs.slice(lastPos, i);
+        lastPos = i + 1;
+        continue;
+      }
+      if (tmpName !== false) {
+        if (c === '"' || c === "'") {
+          var j = attrs.indexOf(c, i + 1);
+          if (j === -1) {
+            break;
+          } else {
+            var v = attrs.slice(lastPos, j + 1).trim();
+            addAttr(tmpName, v);
+            tmpName = false;
+            i = j;
+            lastPos = i + 1;
+            continue;
+          }
+        }
+      } else {
+        if (c === ' ') {
+          var v = attrs.slice(lastPos, i).trim();
+          addAttr(v);
+          tmpName = false;
+          lastPos = i + 1;
+          continue;
+        }
+      }
+    }
+    if (lastPos < attrs.length) {
+      addAttr(attrs.slice(lastPos));
+    }
+    return _attrs.join(' ');
+  };
+
+  /**
+   * 检查标签是否合法
+   */
+  var addNewTag = function (tag, end) {
+    rethtml += noTag(html.slice(lastPos, tagStart));
+    lastPos = end + 1;
+    var spos = tag.slice(0, 2) === '</' ? 2 : 1;
+    
+    var i = tag.indexOf(' ');
+    if (i === -1) {
+      var tagName = tag.slice(spos, tag.length - 1).trim();
+    } else {
+      var tagName = tag.slice(spos, i + 1).trim();
+    }
+    if (tagName in whiteList) {
+      // 过滤不合法的属性
+      if (i === -1) {
+        rethtml += tag.slice(0, spos) + tagName + '>';
+      } else {
+        var attrs = filterAttributes(tag.slice(i + 1, tag.length - 1).trim(), whiteList[tagName]);
+        rethtml += tag.slice(0, spos) + tagName + (attrs.length > 0 ? ' ' + attrs : '') + '>';
+      }
+    } else {
+      // 过滤 <>
+      rethtml += noTag(tag);
+    }
+  };
+
+  // 逐个分析字符
+  for (var i = 0, len = html.length; i < len; i++) {
+    var c = html[i];
+    if (tagStart === false) {
+      if (c === '<') {
+        tagStart = i;
+        continue;
+      }
+    } else {
+      if (quoteStart === false) {
+        if (c === '<') {
+          rethtml += noTag(html.slice(lastPos, i));
+          tagStart = i;
+          lastPos = i;
+          continue;
+        }
+        if (c === '>') {
+          addNewTag(html.slice(tagStart, i + 1), i);
+          tagStart = false;
+          continue;
+        }
+        if (c === '"' || c === "'") {
+          quoteStart = c;
+          continue;
+        }
+      } else {
+        if (c === quoteStart) {
+          quoteStart = false;
+          continue;
+        }
+      }
+    }
+  }
+  if (lastPos < html.length) {
+    rethtml += noTag(html.substr(lastPos));
+  }
+  return rethtml;
+};
+
+exports.whiteList = defaultWhiteList;
---
+++ test/test_xss.js
@@ -0,0 +1,53 @@
+/**
+ * 测试XSS
+ */
+
+var assert = require('assert');
+var xss = require('../');
+
+
+describe('test XSS', function () {
+
+  it('#normal', function () {
+
+    // 过滤不在白名单的标签
+    assert.equal(xss('<b>abcd</b>'), '<b>abcd</b>');
+    assert.equal(xss('<o>abcd</o>'), '&lt;o&gt;abcd&lt;/o&gt;');
+    assert.equal(xss('<b>abcd</o>'), '<b>abcd&lt;/o&gt;');
+    assert.equal(xss('<b><o>abcd</b></o>'), '<b>&lt;o&gt;abcd</b>&lt;/o&gt;');
+    assert.equal(xss('<hr>'), '<hr>');
+    assert.equal(xss('<xss>'), '&lt;xss&gt;');
+    assert.equal(xss('<xss o="x">'), '&lt;xss o="x"&gt;');
+    assert.equal(xss('<a><b>c</b></a>'), '<a><b>c</b></a>');
+    assert.equal(xss('<a><c>b</c></a>'), '<a>&lt;c&gt;b&lt;/c&gt;</a>');
+
+    // 过滤不是标签的<>
+    assert.equal(xss('<>>'), '&lt;&gt;&gt;');
+    assert.equal(xss('<script>'), '&lt;script&gt;');
+    assert.equal(xss('<<a>b>'), '&lt;<a>b&gt;');
+    assert.equal(xss('<<<a>>b</a><x>'), '&lt;&lt;<a>&gt;b</a>&lt;x&gt;');
+
+    // 过滤不再白名单中的属性
+    assert.equal(xss('<a oo="1" xx="2" href="3">yy</a>'), '<a href="3">yy</a>');
+    assert.equal(xss('<a href xx oo>pp</a>'), '<a href>pp</a>');
+    assert.equal(xss('<a href "">pp</a>'), '<a href>pp</a>');
+    assert.equal(xss('<a t="">'), '<a>');
+
+    // 属性内的特殊字符
+    assert.equal(xss('<a href="\'<<>>">'), '<a href="\'<<>>">');
+    assert.equal(xss('<a href="</script>">'), '<a href="&lt;/script&gt;">');
+
+  });
+
+  it('#white list', function () {
+
+    // 过滤所有标签
+    assert.equal(xss('<a href="xx">bb</a>', {}), '&lt;a href="xx"&gt;bb&lt;/a&gt;');
+    assert.equal(xss('<hr>', {}), '&lt;hr&gt;');
+
+    // 增加白名单标签及属性
+    assert.equal(xss('<ooxx yy="ok" cc="no">uu</ooxx>', {ooxx: ['yy']}), '<ooxx yy="ok">uu</ooxx>');
+
+  });
+
+});
