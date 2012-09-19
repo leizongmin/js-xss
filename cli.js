965c5d95e01d6b983f79a8d059099a742e688ca1
leizongmin <leizongmin@gmail.com>
2012-09-19 13:02:59 +0800

    命令行测试工具

---
+++ cli.js
@@ -0,0 +1,34 @@
+/**
+ * 命令行测试工具
+ */
+
+var xss = require('./');
+var readline = require('readline');
+
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+console.log('Enter a blank line to do xss(), enter "@quit" to exit.\n');
+
+
+rl.setPrompt('[1] ');
+rl.prompt();
+
+var html = [];
+rl.on('line', function (line) {
+  if (line === '@quit') return process.exit();
+  if (line === '') {
+    console.log('');
+    console.log(xss(html.join('\r\n')));
+    console.log('');
+    html = [];
+  } else {
+    html.push(line);
+  }
+  rl.setPrompt('[' + (html.length + 1) + '] ');
+  rl.prompt();
+});
