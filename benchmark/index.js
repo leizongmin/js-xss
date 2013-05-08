02cf9113da7353a2d6239d9ef2c3183f63c21797
Lei Zongmin <leizongmin@gmail.com>
2013-05-08 16:14:49 +0800

    性能测试代码

--- benchmark/index.js
+++ benchmark/index.js
@@ -6,19 +6,45 @@ var xss = require('../');
 var fs = require('fs');
 
 
+// 读取样例
 var html = fs.readFileSync(__dirname + '/file.html', 'utf8');
+var COUNT = 200;
+var ret = '';
 
 
 var timeStart = Date.now();
-for (var i = 0; i < 1000; i++) {
-  var ret = xss(html);
+for (var i = 0; i < COUNT; i++) {
+  ret = xss(html);
 }
 var timeEnd = Date.now();
-
-
-//console.log(ret);
-fs.writeFileSync(__dirname + '/result.html', ret);
-
 var spent = timeEnd - timeStart;
 var speed = (((html.length * i) / spent * 1000) / 1024 / 1024).toFixed(2);
-console.log('spent ' + spent + 'ms, ' + speed + 'MB/s');
+console.log('xss(): spent ' + spent + 'ms, ' + speed + 'MB/s');
+
+
+var x = new xss.FilterXSS();
+var timeStart = Date.now();
+for (var i = 0; i < COUNT; i++) {
+  ret = x.process(html);
+}
+var timeEnd = Date.now();
+var spent = timeEnd - timeStart;
+var speed = (((html.length * i) / spent * 1000) / 1024 / 1024).toFixed(2);
+console.log('xss.process(): spent ' + spent + 'ms, ' + speed + 'MB/s');
+
+
+var x = new xss.FilterXSS();
+var process = x.process.bind(x);
+var timeStart = Date.now();
+for (var i = 0; i < COUNT; i++) {
+  ret = process(html);
+}
+var timeEnd = Date.now();
+var spent = timeEnd - timeStart;
+var speed = (((html.length * i) / spent * 1000) / 1024 / 1024).toFixed(2);
+console.log('xss.process() #2: spent ' + spent + 'ms, ' + speed + 'MB/s');
+
+
+// 保存结果
+//console.log(ret);
+fs.writeFileSync(__dirname + '/result.html', ret);
