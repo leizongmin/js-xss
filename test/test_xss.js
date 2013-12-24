ce329b30d8ba7335ee1d1ead3d1848c8a590f86f
Zongmin Lei <leizongmin@gmail.com>
2013-12-24 12:06:52 +0800

    修正html实体转义部分

--- test/test_xss.js
+++ test/test_xss.js
@@ -160,7 +160,7 @@ describe('test XSS', function () {
     //    '<img src="F   M LEJN   ALN      !">');
 
     assert.equal(xss('<IMG SRC=&#x6A&#x61&#x76&#x61&#x73&#x63&#x72&#x69&#x70&#x74&#x3A&#x61&#x6C&#x65&#x72&#x74&#x28&#x27&#x58&#x53&#x53&#x27&#x29>'),
-        '<img src>');
+        '<img src="#">');
 
     assert.equal(xss('<IMG SRC="jav ascript:alert(\'XSS\');">'), '<img src="#">');
 
