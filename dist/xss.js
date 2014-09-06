eb7bcd395a781d8e4d088cbc2ccbe1f09ccb50d7
Zongmin Lei <leizongmin@gmail.com>
2014-09-06 20:25:02 +0800

    v0.1.11

--- dist/xss.js
+++ dist/xss.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * 默认配置
  *
@@ -57,6 +57,8 @@ var whiteList = {
   section:[],
   small:  [],
   span:   [],
+  sub:    [],
+  sup:    [],
   strong: [],
   table:  ['width', 'border', 'align', 'valign'],
   tbody:  ['align', 'valign'],
@@ -787,4 +789,4 @@ FilterXSS.prototype.process = function (html) {
 
 
 module.exports = FilterXSS;
-},{"./default":1,"./parser":3}]},{},[2])
+},{"./default":1,"./parser":3}]},{},[2]);
