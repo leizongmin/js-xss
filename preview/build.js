05e978751493de2963464eef996800dcb3eb6bbe
Zongmin Lei <leizongmin@gmail.com>
2015-01-17 14:45:45 +0800

    修正 preview/build

--- preview/build.js
+++ preview/build.js
@@ -24,29 +24,44 @@ function render (tpl, data, callback) {
 }
 
 
-function registerMarkdown (path, filename) {
+function _registerMarkdown (path, filename) {
   if (path.slice(-1) !== '/') path = path + '.html';
   filename = filename + '.md';
 
   var page = utils.loadPage(filename);
   console.log('register: %s (%s) \t %s', path, filename, page.title);
 
-  render('page', utils.loadPage(filename), function (err, html) {
+  render('page', utils.merge(utils.loadPage(filename), {
+    lang: utils.parseLangFromUrl(path)
+  }), function (err, html) {
+    if (err) throw err;
+    saveFile(path, html);
+  });
+}
+
+function registerMarkdown (path, filename) {
+  utils.lang.forEach(function (lang) {
+    _registerMarkdown('/' + lang + path, lang + '/' + filename);
+  });
+}
+
+function _registerHtml (path, filename, title) {
+  if (path.slice(-1) !== '/') path = path + '.html';
+  filename = filename + '.html';
+
+  render('html', {
+    title: title,
+    html: utils.loadHtml(filename),
+    lang: utils.parseLangFromUrl(path)
+  }, function (err, html) {
     if (err) throw err;
     saveFile(path, html);
   });
 }
 
 function registerHtml (path, filename, title) {
-  if (path.slice(-1) !== '/') path = path + '.html';
-  filename = filename + '.html';
-
-  render('html', {
-    title: title,
-    html: utils.loadHtml(filename)
-  }, function (err, html) {
-    if (err) throw err;
-    saveFile(path, html);
+  utils.lang.forEach(function (lang) {
+    _registerHtml('/' + lang + path, lang + '/' + filename, title);
   });
 }
 
