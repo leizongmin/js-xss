1f7b6f1f9011363e30ff5b23722ebced2c771dc8
Zongmin Lei <leizongmin@gmail.com>
2015-01-17 22:33:41 +0800

    增加页面最后更新时间

--- sources/en/config.js
+++ sources/en/config.js
@@ -1,4 +1,4 @@
 exports.headline = 'Sanitize untrusted HTML (to prevent XSS) with a configuration specified by a Whitelist.';
 exports.footer = {};
 exports.footer.fork = 'Fork the website on GitHub';
-
+exports.footer.page_build = 'Page last updated at:';
--- sources/zh/config.js
+++ sources/zh/config.js
@@ -1,3 +1,4 @@
 exports.headline = '根据白名单过滤HTML(防止XSS攻击)';
 exports.footer = {};
 exports.footer.fork = '在GitHub上修改此网站';
+exports.footer.page_build = '页面最后更新时间:';
