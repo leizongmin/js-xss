9b85b8f2d6c136711b86bbaf91272d792f151bce
Zongmin Lei <leizongmin@gmail.com>
2017-12-21 14:22:34 +0800

    reformat by prettier

--- test/test_custom_method.js
+++ test/test_custom_method.js
@@ -4,65 +4,63 @@
  * @author Zongmin Lei<leizongmin@gmail.com>
  */
 
-var assert = require('assert');
-var xss = require('../');
-var debug = require('debug')('xss:test');
+var assert = require("assert");
+var xss = require("../");
+var debug = require("debug")("xss:test");
 
-
-describe('test custom XSS method', function () {
-
-  it('#onTag - match tag', function () {
+describe("test custom XSS method", function() {
+  it("#onTag - match tag", function() {
     var source = 'dd<a href="#"><b><c>haha</c></b></a><br>ff';
     var i = 0;
     var html = xss(source, {
-      onTag: function (tag, html, options) {
+      onTag: function(tag, html, options) {
         debug(arguments);
         i++;
         if (i === 1) {
-          assert.equal(tag, 'a');
+          assert.equal(tag, "a");
           assert.equal(html, '<a href="#">');
           assert.equal(options.isClosing, false);
           assert.equal(options.position, 2);
           assert.equal(options.sourcePosition, 2);
           assert.equal(options.isWhite, true);
         } else if (i === 2) {
-          assert.equal(tag, 'b');
-          assert.equal(html, '<b>');
+          assert.equal(tag, "b");
+          assert.equal(html, "<b>");
           assert.equal(options.isClosing, false);
           assert.equal(options.position, 14);
           assert.equal(options.sourcePosition, 14);
           assert.equal(options.isWhite, true);
         } else if (i === 3) {
-          assert.equal(tag, 'c');
-          assert.equal(html, '<c>');
+          assert.equal(tag, "c");
+          assert.equal(html, "<c>");
           assert.equal(options.isClosing, false);
           assert.equal(options.position, 17);
           assert.equal(options.sourcePosition, 17);
           assert.equal(options.isWhite, false);
         } else if (i === 4) {
-          assert.equal(tag, 'c');
-          assert.equal(html, '</c>');
+          assert.equal(tag, "c");
+          assert.equal(html, "</c>");
           assert.equal(options.isClosing, true);
           assert.equal(options.position, 30);
           assert.equal(options.sourcePosition, 24);
           assert.equal(options.isWhite, false);
         } else if (i === 5) {
-          assert.equal(tag, 'b');
-          assert.equal(html, '</b>');
+          assert.equal(tag, "b");
+          assert.equal(html, "</b>");
           assert.equal(options.isClosing, true);
           assert.equal(options.position, 40);
           assert.equal(options.sourcePosition, 28);
           assert.equal(options.isWhite, true);
         } else if (i === 6) {
-          assert.equal(tag, 'a');
-          assert.equal(html, '</a>');
+          assert.equal(tag, "a");
+          assert.equal(html, "</a>");
           assert.equal(options.isClosing, true);
           assert.equal(options.position, 44);
           assert.equal(options.sourcePosition, 32);
           assert.equal(options.isWhite, true);
         } else if (i === 7) {
-          assert.equal(tag, 'br');
-          assert.equal(html, '<br>');
+          assert.equal(tag, "br");
+          assert.equal(html, "<br>");
           assert.equal(options.isClosing, false);
           assert.equal(options.position, 48);
           assert.equal(options.sourcePosition, 36);
@@ -73,15 +71,17 @@ describe('test custom XSS method', function () {
       }
     });
     debug(html);
-    assert.equal(html, 'dd<a href="#"><b>&lt;c&gt;haha&lt;/c&gt;</b></a><br>ff');
-
+    assert.equal(
+      html,
+      'dd<a href="#"><b>&lt;c&gt;haha&lt;/c&gt;</b></a><br>ff'
+    );
   });
 
-  it('#onTag - return new html', function () {
+  it("#onTag - return new html", function() {
     var source = 'dd<a href="#"><b><c>haha</c></b></a><br>ff';
     var i = 0;
     var html = xss(source, {
-      onTag: function (tag, html, options) {
+      onTag: function(tag, html, options) {
         debug(html);
         return html;
       }
@@ -90,23 +90,23 @@ describe('test custom XSS method', function () {
     assert.equal(html, source);
   });
 
-  it('#onIgnoreTag - match tag', function () {
+  it("#onIgnoreTag - match tag", function() {
     var source = 'dd<a href="#"><b><c>haha</c></b></a><br>ff';
     var i = 0;
     var html = xss(source, {
-      onIgnoreTag: function (tag, html, options) {
+      onIgnoreTag: function(tag, html, options) {
         debug(arguments);
         i++;
         if (i === 1) {
-          assert.equal(tag, 'c');
-          assert.equal(html, '<c>');
+          assert.equal(tag, "c");
+          assert.equal(html, "<c>");
           assert.equal(options.isClosing, false);
           assert.equal(options.position, 17);
           assert.equal(options.sourcePosition, 17);
           assert.equal(options.isWhite, false);
         } else if (i === 2) {
-          assert.equal(tag, 'c');
-          assert.equal(html, '</c>');
+          assert.equal(tag, "c");
+          assert.equal(html, "</c>");
           assert.equal(options.isClosing, true);
           assert.equal(options.position, 30);
           assert.equal(options.sourcePosition, 24);
@@ -117,45 +117,52 @@ describe('test custom XSS method', function () {
       }
     });
     debug(html);
-    assert.equal(html, 'dd<a href="#"><b>&lt;c&gt;haha&lt;/c&gt;</b></a><br>ff');
+    assert.equal(
+      html,
+      'dd<a href="#"><b>&lt;c&gt;haha&lt;/c&gt;</b></a><br>ff'
+    );
   });
 
-  it('#onIgnoreTag - return new html', function () {
+  it("#onIgnoreTag - return new html", function() {
     var source = 'dd<a href="#"><b><c>haha</c></b></a><br>ff';
     var i = 0;
     var html = xss(source, {
-      onIgnoreTag: function (tag, html, options) {
+      onIgnoreTag: function(tag, html, options) {
         debug(html);
-        return '[' + (options.isClosing ? '/' : '') + 'removed]';
+        return "[" + (options.isClosing ? "/" : "") + "removed]";
       }
     });
     debug(html);
-    assert.equal(html, 'dd<a href="#"><b>[removed]haha[/removed]</b></a><br>ff');
+    assert.equal(
+      html,
+      'dd<a href="#"><b>[removed]haha[/removed]</b></a><br>ff'
+    );
   });
 
-  it('#onTagAttr - match attr', function () {
-    var source = '<a href="#" target="_blank" checked data-a="b">hi</a href="d">';
+  it("#onTagAttr - match attr", function() {
+    var source =
+      '<a href="#" target="_blank" checked data-a="b">hi</a href="d">';
     var i = 0;
     var html = xss(source, {
-      onTagAttr: function (tag, name, value, isWhiteAttr) {
+      onTagAttr: function(tag, name, value, isWhiteAttr) {
         debug(arguments);
-        assert.equal(tag, 'a');
+        assert.equal(tag, "a");
         i++;
         if (i === 1) {
-          assert.equal(name, 'href');
-          assert.equal(value, '#');
+          assert.equal(name, "href");
+          assert.equal(value, "#");
           assert.equal(isWhiteAttr, true);
         } else if (i === 2) {
-          assert.equal(name, 'target');
-          assert.equal(value, '_blank');
+          assert.equal(name, "target");
+          assert.equal(value, "_blank");
           assert.equal(isWhiteAttr, true);
         } else if (i === 3) {
-          assert.equal(name, 'checked');
-          assert.equal(value, '');
+          assert.equal(name, "checked");
+          assert.equal(value, "");
           assert.equal(isWhiteAttr, false);
         } else if (i === 4) {
-          assert.equal(name, 'data-a');
-          assert.equal(value, 'b');
+          assert.equal(name, "data-a");
+          assert.equal(value, "b");
           assert.equal(isWhiteAttr, false);
         } else {
           throw new Error();
@@ -166,34 +173,36 @@ describe('test custom XSS method', function () {
     assert.equal(html, '<a href="#" target="_blank">hi</a>');
   });
 
-  it('#onTagAttr - match attr', function () {
-    var source = '<a href="#" target="_blank" checked data-a="b">hi</a href="d">';
+  it("#onTagAttr - match attr", function() {
+    var source =
+      '<a href="#" target="_blank" checked data-a="b">hi</a href="d">';
     var i = 0;
     var html = xss(source, {
-      onTagAttr: function (tag, name, value, isWhiteAttr) {
+      onTagAttr: function(tag, name, value, isWhiteAttr) {
         debug(arguments);
-        return '$' + name + '$';
+        return "$" + name + "$";
       }
     });
     debug(html);
-    assert.equal(html, '<a $href$ $target$ $checked$ $data-a$>hi</a>');
+    assert.equal(html, "<a $href$ $target$ $checked$ $data-a$>hi</a>");
   });
 
-  it('#onIgnoreTagAttr - match attr', function () {
-    var source = '<a href="#" target="_blank" checked data-a="b">hi</a href="d">';
+  it("#onIgnoreTagAttr - match attr", function() {
+    var source =
+      '<a href="#" target="_blank" checked data-a="b">hi</a href="d">';
     var i = 0;
     var html = xss(source, {
-      onIgnoreTagAttr: function (tag, name, value, isWhiteAttr) {
+      onIgnoreTagAttr: function(tag, name, value, isWhiteAttr) {
         debug(arguments);
-        assert.equal(tag, 'a');
+        assert.equal(tag, "a");
         i++;
         if (i === 1) {
-          assert.equal(name, 'checked');
-          assert.equal(value, '');
+          assert.equal(name, "checked");
+          assert.equal(value, "");
           assert.equal(isWhiteAttr, false);
         } else if (i === 2) {
-          assert.equal(name, 'data-a');
-          assert.equal(value, 'b');
+          assert.equal(name, "data-a");
+          assert.equal(value, "b");
           assert.equal(isWhiteAttr, false);
         } else {
           throw new Error();
@@ -204,136 +213,150 @@ describe('test custom XSS method', function () {
     assert.equal(html, '<a href="#" target="_blank">hi</a>');
   });
 
-  it('#onIgnoreTagAttr - match attr', function () {
-    var source = '<a href="#" target="_blank" checked data-a="b">hi</a href="d">';
+  it("#onIgnoreTagAttr - match attr", function() {
+    var source =
+      '<a href="#" target="_blank" checked data-a="b">hi</a href="d">';
     var i = 0;
     var html = xss(source, {
-      onIgnoreTagAttr: function (tag, name, value, isWhiteAttr) {
+      onIgnoreTagAttr: function(tag, name, value, isWhiteAttr) {
         debug(arguments);
-        return '$' + name + '$';
+        return "$" + name + "$";
       }
     });
     debug(html);
     assert.equal(html, '<a href="#" target="_blank" $checked$ $data-a$>hi</a>');
   });
 
-  it('#escapeHtml - default', function () {
-    var source = '<x>yy</x><a>bb</a>';
+  it("#escapeHtml - default", function() {
+    var source = "<x>yy</x><a>bb</a>";
     var html = xss(source);
     debug(html);
-    assert.equal(html, '&lt;x&gt;yy&lt;/x&gt;<a>bb</a>');
+    assert.equal(html, "&lt;x&gt;yy&lt;/x&gt;<a>bb</a>");
   });
 
-  it('#escapeHtml - return new value', function () {
-    var source = '<x>yy</x><a>bb</a>';
+  it("#escapeHtml - return new value", function() {
+    var source = "<x>yy</x><a>bb</a>";
     var html = xss(source, {
-      escapeHtml: function (str) {
-        return (str ? '[' + str + ']' : str);
+      escapeHtml: function(str) {
+        return str ? "[" + str + "]" : str;
       }
     });
     debug(html);
-    assert.equal(html, '[<x>][yy][</x>]<a>[bb]</a>');
+    assert.equal(html, "[<x>][yy][</x>]<a>[bb]</a>");
   });
 
-  it('#safeAttrValue - default', function () {
+  it("#safeAttrValue - default", function() {
     var source = '<a href="javascript:alert(/xss/)" title="hi">link</a>';
     var html = xss(source);
     debug(html);
     assert.equal(html, '<a href title="hi">link</a>');
   });
 
-  it('#safeAttrValue - return new value', function () {
+  it("#safeAttrValue - return new value", function() {
     var source = '<a href="javascript:alert(/xss/)" title="hi">link</a>';
     var html = xss(source, {
-      safeAttrValue: function (tag, name, value) {
+      safeAttrValue: function(tag, name, value) {
         debug(arguments);
-        assert.equal(tag, 'a');
-        return '$' + name + '$';
+        assert.equal(tag, "a");
+        return "$" + name + "$";
       }
     });
     debug(html);
     assert.equal(html, '<a href="$href$" title="$title$">link</a>');
   });
 
-  it('#stripIgnoreTag', function () {
-    var source = '<x>yy</x><a>bb</a>';
+  it("#stripIgnoreTag", function() {
+    var source = "<x>yy</x><a>bb</a>";
     var html = xss(source, {
       stripIgnoreTag: true
     });
     debug(html);
-    assert.equal(html, 'yy<a>bb</a>');
+    assert.equal(html, "yy<a>bb</a>");
   });
 
-  it('#stripTagBody - true', function () {
-    var source = '<a>link</a><x>haha</x><y>a<y></y>b</y>k';
+  it("#stripTagBody - true", function() {
+    var source = "<a>link</a><x>haha</x><y>a<y></y>b</y>k";
     var html = xss(source, {
       stripIgnoreTagBody: true
     });
     debug(html);
-    assert.equal(html, '<a>link</a>bk');
+    assert.equal(html, "<a>link</a>bk");
   });
 
-  it('#stripIgnoreTagBody - *', function () {
-    var source = '<a>link</a><x>haha</x><y>a<y></y>b</y>k';
+  it("#stripIgnoreTagBody - *", function() {
+    var source = "<a>link</a><x>haha</x><y>a<y></y>b</y>k";
     var html = xss(source, {
-      stripIgnoreTagBody: '*'
+      stripIgnoreTagBody: "*"
     });
     debug(html);
-    assert.equal(html, '<a>link</a>bk');
+    assert.equal(html, "<a>link</a>bk");
   });
 
-  it('#stripIgnoreTagBody - [\'x\']', function () {
-    var source = '<a>link</a><x>haha</x><y>a<y></y>b</y>k';
+  it("#stripIgnoreTagBody - ['x']", function() {
+    var source = "<a>link</a><x>haha</x><y>a<y></y>b</y>k";
     var html = xss(source, {
-      stripIgnoreTagBody: ['x']
+      stripIgnoreTagBody: ["x"]
     });
     debug(html);
-    assert.equal(html, '<a>link</a>&lt;y&gt;a&lt;y&gt;&lt;/y&gt;b&lt;/y&gt;k');
+    assert.equal(html, "<a>link</a>&lt;y&gt;a&lt;y&gt;&lt;/y&gt;b&lt;/y&gt;k");
   });
 
-  it('#stripIgnoreTagBody - [\'x\'] & onIgnoreTag', function () {
-    var source = '<a>link</a><x>haha</x><y>a<y></y>b</y>k';
+  it("#stripIgnoreTagBody - ['x'] & onIgnoreTag", function() {
+    var source = "<a>link</a><x>haha</x><y>a<y></y>b</y>k";
     var html = xss(source, {
-      stripIgnoreTagBody: ['x'],
-      onIgnoreTag: function (tag, html, options) {
-        return '$' + tag + '$';
+      stripIgnoreTagBody: ["x"],
+      onIgnoreTag: function(tag, html, options) {
+        return "$" + tag + "$";
       }
     });
     debug(html);
-    assert.equal(html, '<a>link</a>$y$a$y$$y$b$y$k');
+    assert.equal(html, "<a>link</a>$y$a$y$$y$b$y$k");
   });
 
-  it('#stripIgnoreTag & stripIgnoreTagBody', function () {
-    var source = '<scri' + 'pt>alert(/xss/);</scri' + 'pt>';
+  it("#stripIgnoreTag & stripIgnoreTagBody", function() {
+    var source = "<scri" + "pt>alert(/xss/);</scri" + "pt>";
     var html = xss(source, {
-      stripIgnoreTag:     true,
-      stripIgnoreTagBody: ['script']
+      stripIgnoreTag: true,
+      stripIgnoreTagBody: ["script"]
     });
     debug(html);
-    assert.equal(html, '');
+    assert.equal(html, "");
   });
 
-  it('#stripIgnoreTag & stripIgnoreTagBody - 2', function () {
-    var source = 'ooxx<scri' + 'pt>alert(/xss/);</scri' + 'pt>';
+  it("#stripIgnoreTag & stripIgnoreTagBody - 2", function() {
+    var source = "ooxx<scri" + "pt>alert(/xss/);</scri" + "pt>";
     var html = xss(source, {
-      stripIgnoreTag:     true,
-      stripIgnoreTagBody: ['script']
+      stripIgnoreTag: true,
+      stripIgnoreTagBody: ["script"]
     });
     debug(html);
-    assert.equal(html, 'ooxx');
+    assert.equal(html, "ooxx");
   });
 
-  it('cssFilter', function () {
+  it("cssFilter", function() {
     var whiteList = xss.getDefaultWhiteList();
-    whiteList.div.push('style');
-    assert.equal(xss('<div style="width: 50%; vertical-align: top;">hello</div>', { whiteList: whiteList }),
-                     '<div style="width:50%;">hello</div>');
-    assert.equal(xss('<div style="width: 50%; vertical-align: top;">hello</div>', { whiteList: whiteList, css: false }),
-                     '<div style="width: 50%; vertical-align: top;">hello</div>');
+    whiteList.div.push("style");
+    assert.equal(
+      xss('<div style="width: 50%; vertical-align: top;">hello</div>', {
+        whiteList: whiteList
+      }),
+      '<div style="width:50%;">hello</div>'
+    );
+    assert.equal(
+      xss('<div style="width: 50%; vertical-align: top;">hello</div>', {
+        whiteList: whiteList,
+        css: false
+      }),
+      '<div style="width: 50%; vertical-align: top;">hello</div>'
+    );
     var css = { whiteList: xss.getDefaultCSSWhiteList() };
-    css.whiteList['vertical-align'] = true;
-    assert.equal(xss('<div style="width: 50%; vertical-align: top;">hello</div>', { whiteList: whiteList, css: css }),
-                     '<div style="width:50%; vertical-align:top;">hello</div>');
+    css.whiteList["vertical-align"] = true;
+    assert.equal(
+      xss('<div style="width: 50%; vertical-align: top;">hello</div>', {
+        whiteList: whiteList,
+        css: css
+      }),
+      '<div style="width:50%; vertical-align:top;">hello</div>'
+    );
   });
-
 });
--- test/test_html_parser.js
+++ test/test_html_parser.js
@@ -4,124 +4,136 @@
  * @author Zongmin Lei<leizongmin@gmail.com>
  */
 
-var assert = require('assert');
-var parser = require('../lib/parser');
+var assert = require("assert");
+var parser = require("../lib/parser");
 var parseTag = parser.parseTag;
 var parseAttr = parser.parseAttr;
-var debug = require('debug')('xss:test');
+var debug = require("debug")("xss:test");
 
-describe('test HTML parser', function () {
-
-  function escapeHtml (html) {
-    return html.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+describe("test HTML parser", function() {
+  function escapeHtml(html) {
+    return html.replace(/</g, "&lt;").replace(/>/g, "&gt;");
   }
 
-  function attr (n, v) {
+  function attr(n, v) {
     if (v) {
-      return n + '="' + v.replace(/"/g, '&quote;') + '"';
+      return n + '="' + v.replace(/"/g, "&quote;") + '"';
     } else {
       return n;
     }
   }
 
-  it('#parseTag', function () {
+  it("#parseTag", function() {
     var i = 0;
-    var html = parseTag('hello<A href="#">www</A>ccc<b><br/>', function (sourcePosition, position, tag, html, isClosing) {
-      i++;
-      debug(arguments);
-      if (i === 1) {
-        // 第1个标签
-        assert.equal(sourcePosition, 5);
-        assert.equal(position, 5);
-        assert.equal(tag, 'a');
-        assert.equal(html, '<A href="#">');
-        assert.equal(isClosing, false);
-        return '[link]';
-      } else if (i === 2) {
-        // 第2个标签
-        assert.equal(sourcePosition, 20);
-        assert.equal(position, 14);
-        assert.equal(tag, 'a');
-        assert.equal(html, '</A>');
-        assert.equal(isClosing, true);
-        return '[/link]';
-      } else if (i === 3) {
-        // 第3个标签
-        assert.equal(sourcePosition, 27);
-        assert.equal(position, 24);
-        assert.equal(tag, 'b');
-        assert.equal(html, '<b>');
-        assert.equal(isClosing, false);
-        return '[B]';
-      } else if (i === 4) {
-        // 第4个标签
-        assert.equal(sourcePosition, 30);
-        assert.equal(position, 27);
-        assert.equal(tag, 'br');
-        assert.equal(html, '<br/>');
-        assert.equal(isClosing, false);
-        return '[BR]';
-      } else {
-        throw new Error();
-      }
-    }, escapeHtml);
+    var html = parseTag(
+      'hello<A href="#">www</A>ccc<b><br/>',
+      function(sourcePosition, position, tag, html, isClosing) {
+        i++;
+        debug(arguments);
+        if (i === 1) {
+          // 第1个标签
+          assert.equal(sourcePosition, 5);
+          assert.equal(position, 5);
+          assert.equal(tag, "a");
+          assert.equal(html, '<A href="#">');
+          assert.equal(isClosing, false);
+          return "[link]";
+        } else if (i === 2) {
+          // 第2个标签
+          assert.equal(sourcePosition, 20);
+          assert.equal(position, 14);
+          assert.equal(tag, "a");
+          assert.equal(html, "</A>");
+          assert.equal(isClosing, true);
+          return "[/link]";
+        } else if (i === 3) {
+          // 第3个标签
+          assert.equal(sourcePosition, 27);
+          assert.equal(position, 24);
+          assert.equal(tag, "b");
+          assert.equal(html, "<b>");
+          assert.equal(isClosing, false);
+          return "[B]";
+        } else if (i === 4) {
+          // 第4个标签
+          assert.equal(sourcePosition, 30);
+          assert.equal(position, 27);
+          assert.equal(tag, "br");
+          assert.equal(html, "<br/>");
+          assert.equal(isClosing, false);
+          return "[BR]";
+        } else {
+          throw new Error();
+        }
+      },
+      escapeHtml
+    );
     debug(html);
-    assert.equal(html, 'hello[link]www[/link]ccc[B][BR]');
+    assert.equal(html, "hello[link]www[/link]ccc[B][BR]");
   });
 
-  it('#parseAttr', function () {
+  it("#parseAttr", function() {
     var i = 0;
-    var html = parseAttr('href="#"attr1=b attr2=c attr3 attr4=\'value4"\'attr5/', function (name, value) {
-      i++;
-      debug(arguments);
-      if (i === 1) {
-        assert.equal(name, 'href');
-        assert.equal(value, '#');
-        return attr(name, value);
-      } else if (i === 2) {
-        assert.equal(name, 'attr1');
-        assert.equal(value, 'b');
-        return attr(name, value);
-      } else if (i === 3) {
-        assert.equal(name, 'attr2');
-        assert.equal(value, 'c');
-        return attr(name, value);
-      } else if (i === 4) {
-        assert.equal(name, 'attr3');
-        assert.equal(value, '');
-        return attr(name, value);
-      } else if (i === 5) {
-        assert.equal(name, 'attr4');
-        assert.equal(value, 'value4"');
-        return attr(name, value);
-      } else if (i === 6) {
-        assert.equal(name, 'attr5');
-        assert.equal(value, '');
-        return attr(name, value);
-      } else {
-        throw new Error();
+    var html = parseAttr(
+      'href="#"attr1=b attr2=c attr3 attr4=\'value4"\'attr5/',
+      function(name, value) {
+        i++;
+        debug(arguments);
+        if (i === 1) {
+          assert.equal(name, "href");
+          assert.equal(value, "#");
+          return attr(name, value);
+        } else if (i === 2) {
+          assert.equal(name, "attr1");
+          assert.equal(value, "b");
+          return attr(name, value);
+        } else if (i === 3) {
+          assert.equal(name, "attr2");
+          assert.equal(value, "c");
+          return attr(name, value);
+        } else if (i === 4) {
+          assert.equal(name, "attr3");
+          assert.equal(value, "");
+          return attr(name, value);
+        } else if (i === 5) {
+          assert.equal(name, "attr4");
+          assert.equal(value, 'value4"');
+          return attr(name, value);
+        } else if (i === 6) {
+          assert.equal(name, "attr5");
+          assert.equal(value, "");
+          return attr(name, value);
+        } else {
+          throw new Error();
+        }
       }
-    });
+    );
     debug(html);
-    assert.equal(html, 'href="#" attr1="b" attr2="c" attr3 attr4="value4&quote;" attr5');
+    assert.equal(
+      html,
+      'href="#" attr1="b" attr2="c" attr3 attr4="value4&quote;" attr5'
+    );
   });
 
-  it('#parseTag & #parseAttr', function () {
-    var html = parseTag('hi:<a href="#"target=_blank title="this is a link">link</a>', function (sourcePosition, position, tag, html, isClosing) {
-      if (tag === 'a') {
-        if (isClosing) return '</a>';
-        var attrhtml = parseAttr(html.slice(2, -1), function (name, value) {
-          if (name === 'href' || name === 'target') {
-            return attr(name, value);
-          }
-        });
-        return '<a ' + attrhtml + '>';
-      } else {
-        return escapeHtml(html);
-      }
-    }, escapeHtml);
+  it("#parseTag & #parseAttr", function() {
+    var html = parseTag(
+      'hi:<a href="#"target=_blank title="this is a link">link</a>',
+      function(sourcePosition, position, tag, html, isClosing) {
+        if (tag === "a") {
+          if (isClosing) return "</a>";
+          var attrhtml = parseAttr(html.slice(2, -1), function(name, value) {
+            if (name === "href" || name === "target") {
+              return attr(name, value);
+            }
+          });
+          return "<a " + attrhtml + ">";
+        } else {
+          return escapeHtml(html);
+        }
+      },
+      escapeHtml
+    );
     debug(html);
     assert.equal(html, 'hi:<a href="#" target="_blank">link</a>');
   });
-
 });
